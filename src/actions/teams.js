ad8bcc7527c78cdea920d42ebc13923749b30b8b
3nt3 <gott@3nt3.de>
2019-08-03 18:28:41 +0200

    teams

--- src/actions/teams.js
+++ src/actions/teams.js
@@ -1,5 +1,12 @@
-import { GET_TEAM } from "./types";
+import {
+  GET_TEAM,
+  ADD_USER_TO_TEAM,
+  CREATE_TEAM,
+  SET_IS_TEAM_MEMBER
+} from "./types";
 import axios from "axios";
+
+import store from "../store";
 
 // GET_TEAM
 export const getTeam = () => dispatch => {
@@ -8,9 +15,45 @@ export const getTeam = () => dispatch => {
       withCredentials: true
     })
     .then(res => {
-        dispatch({
-            type: GET_TEAM,
-            payload: res.data
-        })
+      dispatch({
+        type: GET_TEAM,
+        payload: res.data
+      })
     })
 };
+
+export const createTeam = (name) => dispatch => {
+  axios
+    .post("http://localhost:8000/team/", JSON.stringify({
+      name
+    }), {
+      withCredentials: true
+    })
+    .then(res => {
+      console.log(res.data)
+
+      dispatch({
+        type: CREATE_TEAM,
+        payload: res.data
+      })
+      store.dispatch({
+        type: SET_IS_TEAM_MEMBER,
+        payload: !false
+      })
+    })
+
+}
+
+// ADD_USER_TO_TEAM
+export const addUserToTeam = (username) => dispatch => {
+  axios
+    .post("http://localhost:8000/team/addMember/", JSON.stringify(username), {
+      withCredentials: true
+    })
+    .then(res => {
+      dispatch({
+        type: ADD_USER_TO_TEAM,
+        payload: res.data
+      })
+    })
+}
--- src/components/dashboard/Team.js
+++ src/components/dashboard/Team.js
@@ -2,7 +2,7 @@ import React, { Component } from "react";
 import PropTypes from "prop-types";
 import { connect } from "react-redux";
 
-import { getTeam } from "../../actions/teams";
+import { getTeam, addUserToTeam, createTeam } from "../../actions/teams";
 
 const TeamMemberList = props => {
   if (props.members === undefined) {
@@ -31,29 +31,102 @@ export class Team extends Component {
     team: PropTypes.object.isRequired
   };
 
+ 
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      form: {
+        username: ""
+      },
+      createTeamForm: {
+        name: ""
+      }
+    };
+  this.handleChange = this.handleChange.bind(this);
+  this.handleSubmit = this.handleSubmit.bind(this);
+  
+  this.createTeamChange = this.createTeamChange.bind(this);
+  this.createTeamSubmit = this.createTeamSubmit.bind(this);
+
+  }
+
+  componentWillReceiveProps(nextProps) {
+    console.log(nextProps.team)
+  }
+
   componentDidMount() {
     this.props.getTeam();
   }
 
+  handleChange(e) {
+    this.setState({ form: { [e.target.id]: e.target.value } });
+  }
+
+  handleSubmit(e) {
+    e.preventDefault();
+    this.props.addUserToTeam(this.state.form.username);
+  }
+
+  createTeamChange(e) {
+    this.setState({
+      createTeamForm: {
+        [e.target.id]: e.target.value
+      }
+    });
+  }
+
+  createTeamSubmit(e) {
+    e.preventDefault();
+    this.props.createTeam(this.state.createTeamForm.name);
+  }
+
   render() {
-    if (this.props.team.team != undefined) {
+    if (!this.props.isTeamMember) {
+      return (
+        <div>
+          <h3 className="container-headline">you are not part of a team.</h3>
+          <p>
+            loneliness is unhealthy. join a team or create one yourself (つ ͡° ͜ʖ
+            ͡°)つ
+          </p>
+          <div>
+            <form onSubmit={this.createTeamSubmit}>
+              <input
+                placeholder="enter a name for your team"
+                id="name"
+                onChange={this.createTeamChange}
+              />
+              <input type="submit" />
+            </form>
+          </div>
+        </div>
+      );
+    }
+    if (this.props.team.team !== undefined) {
       return (
         <div>
           <h3 className="container-headline">
             team -
-            <span style={{color: "black"}}>
+            <span style={{ color: "black" }}>
               {this.props.team.team.name} by user #{this.props.team.team.owner}
             </span>
           </h3>
           <div className="team-members">
             <TeamMemberList members={this.props.team.members} />
           </div>
+          <div className="add-user-form">
+            <form onSubmit={this.handleSubmit}>
+              <input id="username" onChange={this.handleChange} />
+              <input type="submit" />
+            </form>
+          </div>
         </div>
       );
     } else {
       return (
         <div>
-          <h3 className="container-headline">loading..</h3>
+          <h3 className="container-headline">loading...</h3>
         </div>
       );
     }
@@ -61,11 +134,14 @@ export class Team extends Component {
 }
 
 const mapStateToProps = state => ({
-  team: state.teams.team
+  team: state.teams.team,
+  isTeamMember: state.auth.user.team_member
 });
 
 const mapDispatchToProps = {
-  getTeam
+  getTeam,
+  addUserToTeam,
+  createTeam
 };
 
 export default connect(
