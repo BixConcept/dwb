3202c8ca306264fd45c9018245792a924228a7a2
3nt3 <gott@3nt3.de>
2019-08-03 13:20:51 +0200

    fix user creation

--- backend/db/user.go
+++ backend/db/user.go
@@ -18,7 +18,7 @@ func CreateUser(username, password string, permission int) (int, error) {
 
 	user.PasswordHash = hashedPass
 
-	query := "INSERT INTO users VALUES (default, $1, $2, $3) returning id;"
+	query := "INSERT INTO users  VALUES (default, $1, $2, 0, false, $3) returning id;"
 	row := Database.QueryRow(query, username, hashedPass, permission)
 
 	id := 0
