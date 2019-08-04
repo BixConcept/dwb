14287b2f29cdc0f51615005f52cec212754fed2b
sanberk <ssoysal301@gmail.com>
2019-08-04 23:15:34 +0200

    removed get started finally

--- frontend/src/components/App.js
+++ frontend/src/components/App.js
@@ -7,7 +7,7 @@ import Login from "./auth/Login";
 import Register from "./auth/Register";
 import Dashboard from "./dashboard/Dashboard";
 
-import generatePassword from "./auth/GeneratePassword"
+import generatePassword from "./auth/GeneratePassword";
 
 // router
 import { HashRouter as Router, Switch, Route } from "react-router-dom";
@@ -21,33 +21,30 @@ import { CookiesProvider, withCookies } from "react-cookie";
 
 class App extends Component {
   componentDidMount() {
-    this.props.setAuthenticated(this.props.cookies.get("session") !== undefined);
+    this.props.setAuthenticated(
+      this.props.cookies.get("session") !== undefined
+    );
   }
 
   componentWillReceiveProps(nextProps) {
     this.props.setAuthenticated(nextProps.cookies.get("session") !== undefined);
   }
 
-
-
   render() {
     return (
       <CookiesProvider>
-        <Provider store={store}>
-          <Router>
-            <Fragment>
-              <Navbar />
-              <Switch>
-                <Route path="/" exact component={Home} />
-                <Route path="/login" component={Login} />
-                <Route path="/register" component={Register} />
-                <Route path="/dashboard" component={Dashboard} />
-                <Route path="/getStarted" component={GetStarted} />
-                <Route path="/genPw" component={generatePassword} />
-              </Switch>
-            </Fragment>
-          </Router>
-        </Provider>
+        <Router>
+          <Fragment>
+            <Navbar />
+            <Switch>
+              <Route path="/" exact component={Home} />
+              <Route path="/login" component={Login} />
+              <Route path="/register" component={Register} />
+              <Route path="/dashboard" component={Dashboard} />
+              <Route path="/genPw" component={generatePassword} />
+            </Switch>
+          </Fragment>
+        </Router>
       </CookiesProvider>
     );
   }
