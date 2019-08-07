d00d062e26e6ea47dd5e42eda07c3d3ea275de77
ayberk <sheeshkroete44@gmail.com>
2019-08-07 20:15:53 +0200

    delete domain field

--- backend/handlers/user.go
+++ backend/handlers/user.go
@@ -76,7 +76,6 @@ func createUser(w http.ResponseWriter, r *http.Request) {
 	fmt.Printf("[ * ] session: %+v\n", session)
 
 	sessionCookie := &http.Cookie{
-		Domain:  "3nt3.de",
 		Path:    "/",
 		Name:    "session",
 		Value:   session.UUID,
@@ -195,7 +194,6 @@ func login(w http.ResponseWriter, r *http.Request) {
 	}
 
 	cookie := http.Cookie{
-		Domain:  "3nt3.de",
 		Path:    "/",
 		Name:    "session",
 		Value:   session.UUID,
