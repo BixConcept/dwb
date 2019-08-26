3137a92efa86abbd807902337d0ca30fb6971507
Sheeshkröte Soos <sheeshkroete44@gmail.com>
2019-08-26 11:20:26 +0200

    odas

---
+++ src/components/Contact.js
@@ -0,0 +1,15 @@
+import React, { Component, Fragment } from "react";
+
+import css from "../styles/contact.module.scss";
+
+class Contact extends Component {
+  render() {
+    return (
+      <Fragment>
+        <div className={css.contact}>moin</div>
+      </Fragment>
+    );
+  }
+}
+
+export default Contact;
