9ae09cf38b190b124ab00465c41be05b4ebec308
Sheeshkröte Soos <sheeshkroete44@gmail.com>
2019-08-26 11:20:26 +0200

    odas

--- frontend/src/components/dashboard/Home.js
+++ frontend/src/components/dashboard/Home.js
@@ -1,37 +1,34 @@
-import React, {Component} from 'react';
+import React, { Component } from "react";
 
-import css from '../../styles/dashboard/home/home.module.scss'
-
+import css from "../../styles/dashboard/home/home.module.scss";
 
 import CreateAssignmentForm from "./CreateAssignmentForm";
-import TeamWidget from "./TeamWidget"
-import AssignmentsWidget from "./AssignmentsWidget"
+import TeamWidget from "./TeamWidget";
+import AssignmentsWidget from "./AssignmentsWidget";
 
 class Home extends Component {
-  static
-
   render() {
     return (
       <div className={css.wrapper}>
         <section className={css.create}>
           <h1 className="s-heading">create assignment</h1>
-          <CreateAssignmentForm/>
+          <CreateAssignmentForm />
         </section>
         <section className={css.team}>
           <h1 className="s-heading">team</h1>
-          <TeamWidget/>
+          <TeamWidget />
         </section>
         <section className={css.stats}>
           <h1 className="s-heading">stats</h1>
 
           <div>
-            <h2 className={css.bigNumber}>{this.state.assignmentsDue}</h2>
+            <h2 className={css.bigNumber}>2</h2>
             <h3>assignments due</h3>
           </div>
         </section>
-        <AssignmentsWidget/>
+        <AssignmentsWidget />
       </div>
-    )
+    );
   }
 }
 
