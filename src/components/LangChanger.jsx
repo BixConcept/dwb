163779a65a11da3bf6149e691a9fed537a028bb2
Sheeshkröte Soos <sheeshkroete44@gmail.com>
2019-08-28 18:56:54 +0200

    feat: language selector dropdown button (contains some bugs and stuff)

--- src/components/LangChanger.jsx
+++ src/components/LangChanger.jsx
@@ -1,63 +1,59 @@
 import React, { Component } from "react";
-import { withTranslation } from "react-i18next";
+import { withTranslation, I18nextProvider } from "react-i18next";
 import ReactDOM from "react-dom";
 
 import css from "../styles/langChanger.module.scss";
 class LangChanger extends Component {
   constructor(props) {
     super(props);
-    this.handleChange = this.handleChange.bind(this);
-  }
 
-  handleChange(e) {
-    let selectedIndex = e.target.options.selectedIndex;
-    console.log(selectedIndex);
-
-    let language = "";
-    console.log(this.props.i18n);
-    switch (selectedIndex) {
-      case 0:
-        language = "de";
-        break;
-      case 1:
-        language = "en";
-        break;
-    }
-    this.props.i18n.changeLanguage(language);
-  }
-
-  componentDidMount() {
-    window.onclick = event => {
-      if (!event.target.matches(css.dropdownBtn)) {
-        let dropdowns = document.getElementsByClassName(css.dropwdownContent);
-        for (let i = 0; i < dropdowns.length; i++) {
-          let openDropdown = dropdowns[i];
-          if (openDropdown.classList.contains(css.show)) {
-            openDropdown.classList.remove(css.show);
-          }
-        }
-      }
+    this.state = {
+      language: ""
     };
   }
+
+  changeLng(lng) {
+    console.log("moin");
+    this.setState({ language: lng });
+    console.log("successfully setted");
+    this.props.i18n.changeLanguage(this.state.language);
+  }
+
   render() {
     const { t } = this.props;
+
     return (
-      <div className={css.dropdown}>
-        <button
-          onClick={() => {
-            console.log("btn clicked");
-            let d = ReactDOM.findDOMNode(this.refs.dropdwnContent);
-            d.classList.toggle(css.show);
-          }}
-          className={css.dropdownBtn}
-        >
-          {t("language")}
-        </button>
-        <div ref={"dropdwnContent"} className={css.dropdownContent}>
-          <p href="#">englisch</p>
-          <p href="#">deutsch</p>
+      <I18nextProvider>
+        <div className={css.dropdown} id="moin">
+          <button
+            onClick={() => {
+              let d = ReactDOM.findDOMNode(this.refs.dropdwnContent);
+              d.classList.toggle(css.show);
+            }}
+            className={css.dropdownBtn}
+          >
+            {t("language")}
+          </button>
+          <div ref={"dropdwnContent"} className={css.dropdownContent}>
+            <p
+              onClick={() => {
+                console.log("en");
+                this.changeLng("en");
+              }}
+            >
+              englisch
+            </p>
+            <p
+              onClick={() => {
+                console.log("de");
+                this.changeLng("de");
+              }}
+            >
+              deutsch
+            </p>
+          </div>
         </div>
-      </div>
+      </I18nextProvider>
     );
   }
 }
