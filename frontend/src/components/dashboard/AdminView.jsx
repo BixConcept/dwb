67e56042945698dd9e1f2d6aaa9a8fde1ad4d26f
3nt3 <gott@3nt3.de>
2019-09-04 19:40:51 +0200

    add boilerplate and links

---
+++ frontend/src/components/dashboard/AdminView.jsx
@@ -0,0 +1,21 @@
+import React, { Component, Fragment } from 'react'
+
+class AdminView extends Component {
+    constructor(props) {
+        super(props)
+
+        this.state = {
+            
+        }
+    }
+
+    render() {
+        return (
+            <Fragment>
+                <h1>admin dashboard</h1>
+            </Fragment>
+        )
+    }
+}
+
+export default AdminView
