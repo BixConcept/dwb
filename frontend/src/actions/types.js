3909a1f62195a240f7c6ae79e2376b6254130a23
3nt3 <gott@3nt3.de>
2019-09-07 23:30:29 +0200

    redux code

--- frontend/src/actions/types.js
+++ frontend/src/actions/types.js
@@ -1,6 +1,7 @@
 export const GET_ASSIGNMENTS = "GET_ASSIGNMENTS";
 export const CREATE_ASSIGNMENT = "CREATE_ASSIGNMENT";
 export const DELETE_ASSIGNMENT = "DELETE_ASSIGNMENT";
+export const MARK_FINISHED = "MARK_FINISHED";
 
 export const LOGIN = "LOGIN";
 export const REGISTER = "REGISTER";
@@ -10,7 +11,7 @@ export const SET_IS_TEAM_MEMBER = "SET_IS_TEAM_MEMBER";
 
 export const GET_TEAM = "GET_TEAM";
 export const ADD_USER_TO_TEAM = "ADD_USER_TO_TEAM";
-export const CREATE_TEAM = "CREATE_TEAM"
+export const CREATE_TEAM = "CREATE_TEAM";
 
 // ERRORS
-export const GET_ERROR = "GET_ERROR"
+export const GET_ERROR = "GET_ERROR";
