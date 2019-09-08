28a9f1fed6952f4444c953f4c0cb8fdd966cbaa1
ente <gott@3nt3.de>
2019-09-08 18:55:59 +0000

    Revert "Merge branch 'staging' into 'master'"
    This reverts merge request !11

--- frontend/src/reducers/auth.js
+++ frontend/src/reducers/auth.js
@@ -3,8 +3,7 @@ import {
   REGISTER,
   GET_USER,
   SET_AUTHENTICATED,
-  SET_IS_TEAM_MEMBER,
-  GET_ALL_USERS
+  SET_IS_TEAM_MEMBER
 } from "../actions/types";
 
 const initialState = {
@@ -38,11 +37,6 @@ export default function(state = initialState, action) {
       return {
         ...state,
         user: { team_member: action.payload }
-      };
-    case GET_ALL_USERS:
-      return {
-        ...state,
-        users: action.payload
       };
     default:
       return state;
