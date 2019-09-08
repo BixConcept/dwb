ff9b2adca22940b7f853e92afe6bbe2f5dcc5bc8
3nt3 <gott@3nt3.de>
2019-09-08 21:13:35 +0200

    add build files

---
+++ frontend/build/precache-manifest.07bf0e617248ba054b1a2d0818a2b274.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "01e17916052ac99d06dfa22f17df655b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "8f3f71b81545193aa8b7",
+    "url": "/static/css/main.19feb3bf.chunk.css"
+  },
+  {
+    "revision": "b60ce0b87502114ab1c8",
+    "url": "/static/js/2.415139dd.chunk.js"
+  },
+  {
+    "revision": "8f3f71b81545193aa8b7",
+    "url": "/static/js/main.1ec20880.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "50e95ae401d43883d955308d3e04fd66",
+    "url": "/static/media/bg.50e95ae4.jpg"
+  },
+  {
+    "revision": "af55af54de4946eccf5b523a349dc291",
+    "url": "/static/media/team.af55af54.jpg"
+  }
+]);
