895994112cd8e674c389cd6b882f74e16fe99695
3nt3 <gott@3nt3.de>
2019-09-17 19:02:11 +0200

    add hander

--- backend/handlers/contact.go
+++ backend/handlers/contact.go
@@ -1,15 +1,34 @@
 package handlers
 
-import "net/http"
+import (
+	"encoding/json"
+	"fmt"
+	"net/http"
 
+	"gitlab.com/3nt3rt41nm3nt-gbr/dwb/contact"
+	"gitlab.com/3nt3rt41nm3nt-gbr/dwb/structs"
+)
 
 // Contact handles all requests to contact endpoint
 func Contact(w http.ResponseWriter, r *http.Request) {
-	
-	createMessage(w,r)
-
+	createMessage(w, r)
 }
 
 func createMessage(w http.ResponseWriter, r *http.Request) {
-	
+	message := structs.ContactMessage{}
+	err := json.NewDecoder(r.Body).Decode(&message)
+	if err != nil {
+		fmt.Printf("[ - ] error decoding body: %v\n", err)
+		w.WriteHeader(400)
+		return
+	}
+
+	err = contact.CreateMessage(message)
+	if err != nil {
+		fmt.Printf("[ - ] error sending message: %v\n", err)
+		w.WriteHeader(500)
+		return
+	}
+
+	fmt.Printf("[ + ] successfully sent message.\n")
 }
