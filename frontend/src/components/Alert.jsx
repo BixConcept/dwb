0ef36ade16068152cfd6193ff65c0fb24367ba86
3nt3 <gott@3nt3.de>
2019-10-31 16:19:49 +0000

    make errors work

--- frontend/src/components/Alert.jsx
+++ frontend/src/components/Alert.jsx
@@ -1,12 +1,12 @@
 import React, { Component } from "react";
 
 // css
-import "./Alert.css";
+import css from "../styles/alert.module.scss";
 
 export default class Alert extends Component {
   render() {
     return (
-      <div className={`alert alert-${this.props.type}`}>
+      <div className={css.alert}>
         <h3>{this.props.title}</h3>
         <p>{this.props.text}</p>
       </div>
