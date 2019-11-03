d0415864cfa5cf6460ead8be4961e4ccea395a40
3nt3 <gott@3nt3.de>
2019-11-03 22:37:37 +0000

    i don't really know

--- frontend/src/components/dashboard/TeamMessage.jsx
+++ frontend/src/components/dashboard/TeamMessage.jsx
@@ -1,17 +1,13 @@
 import React, { Component } from "react";
 import { connect } from "react-redux";
-import css from "../../styles/dashboard/home/teamMessageWidget.module.scss"
+import css from "../../styles/dashboard/home/teamMessageWidget.module.scss";
 
 export class TeamMessage extends Component {
   render() {
     if (this.props.team.team !== undefined) {
-      return (
-        <div className={css.messageContainer}>
-            {this.props.team.team.message}
-        </div>
-      );
+      return <div className={css.messageContainer}>{this.props.team.team.message}</div>;
     } else {
-      return null
+      return null;
     }
   }
 }
