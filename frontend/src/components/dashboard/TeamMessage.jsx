4aaa431223c0e57511e26c3f403e4e2d1fd8f421
sheeshkroete44 <sheeshkroete44@gmail.com>
2019-11-07 22:32:48 +0100

    add quotes

--- frontend/src/components/dashboard/TeamMessage.jsx
+++ frontend/src/components/dashboard/TeamMessage.jsx
@@ -1,17 +1,31 @@
 import React, { Component } from "react";
 import { connect } from "react-redux";
-import css from "../../styles/dashboard/home/teamMessageWidget.module.scss"
+import css from "../../styles/dashboard/home/teamMessageWidget.module.scss";
+
+const Quote = () => {
+  let quotes = [
+    "Jungs, lasst den Schwanz in Ruhe! - Fr. Dr. Horn",
+    "a² * b² = c² - GZUZ",
+    "Du Internetrambo! - Gebrüder Grimm",
+    "DEUTSCHLAND braucht einen Platz oben inna Süd. - Wilhelm II.",
+    "An der Nase eines Mannes erkennt man seinen Johannes. - Immanuel Kant",
+    "Hallo. - Jesus Christus"
+  ];
+  let getRndmQuote = quotes[Math.floor(Math.random() * quotes.length)];
+  return getRndmQuote;
+};
 
 export class TeamMessage extends Component {
   render() {
     if (this.props.team.team !== undefined) {
       return (
         <div className={css.messageContainer}>
-            {this.props.team.team.message}
+          {/*{this.props.team.team.message}*/}
+          <Quote />
         </div>
       );
     } else {
-      return null
+      return null;
     }
   }
 }
