06992590a9b53e1a2eda3d3cd57b93208440120d
3nt3 <gott@3nt3.de>
2019-12-01 16:07:39 +0100

    update quotes

--- frontend/src/components/dashboard/TeamMessage.jsx
+++ frontend/src/components/dashboard/TeamMessage.jsx
@@ -22,17 +22,17 @@ function changeText() {
 } */
 const Quote = () => {
   let quotes = [
-    "Jungs, lasst den Schwanz in Ruhe! - Fr. Dr. Horn",
-    "a² * b² = c² - GZUZ",
-    "Du Internetrambo! - Gebrüder Grimm",
-    "Deutschland braucht einen Platz an der Sonne. - Wilhelm II.",
-    "An der Nase eines Mannes erkennt man seinen Johannes. - Immanuel Kant",
-    "Hallo. - Jesus Christus",
-    "In Mexiko sagt man nicht 'Ich liebe dich.' die können dort nämlich gar kein Deutsch. - Plato",
-    "Die Sieger Olympias bekamen goldene Lorbeerkränze. - David K.",
-    "Smoke trees everyday. - David K.",
-    "In germany teachers don't say 'you have to learn this'. Instead they say: 'Wenn ich um 3 Uhr nachts bei dir am Bett stehe und ich dich das abfrage musst du das, wie aus der Pistole geschossen, im Halbschlaf beantworten können ohne nachzudenken.' - Jeder Lateinlehrer",
-    "Niels, du bist länger und größer. - Friedrich III."
+    "Jungs, hört auf mit dem Schwanz zu spielen -- Fr. Dr. Horn zu den Jungen des Biologiekurses",
+    "a² * b² = c² -- GZUZ",
+    "Du Internetrambo! -- Gebrüder Grimm",
+    "Deutschland braucht einen Platz an der Sonne. -- Wilhelm II.",
+    "An der Nase eines Mannes erkennt man seinen Johannes. -- Immanuel Kant",
+    "Hallo. -- Jesus Christus",
+    "In Mexiko sagt man nicht 'Ich liebe dich.' die können dort nämlich gar kein Deutsch. -- Plato",
+    "Die Sieger Olympias bekamen goldene Lorbeerkränze. -- David K.",
+    "Smoke trees everyday. -- Sigmund Freud",
+    "In germany teachers don't say 'you have to learn this'. Instead they say: 'Wenn ich um 3 Uhr nachts bei dir am Bett stehe und ich dich das abfrage musst du das, wie aus der Pistole geschossen, im Halbschlaf beantworten können ohne nachzudenken.' -- Jeder Lateinlehrer",
+    "Niels, du bist länger und größer. -- Friedrich III."
     ];
   let getRndmQuote = quotes[Math.floor(Math.random() * quotes.length)];
   return <p id="quote">{getRndmQuote}</p>;
