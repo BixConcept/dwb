158bd4754cfc9d823c9842a9d024bb6dbd5b08b0
3nt3 <gott@3nt3.de>
2020-03-08 21:57:44 +0100

    add build files

--- build/service-worker.js
+++ build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.0f249b58253f6dc5893f27254b17e196.js"
+  "/precache-manifest.534af1cde5d2c9fed660ddbeb02e97a0.js"
 );
 
 self.addEventListener('message', (event) => {
