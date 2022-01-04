dad22213e0be6dda4fa0176521a7a579194c00b1
3nt3 <gott@3nt3.de>
2022-01-04 06:51:42 +0100

    Fix langChanger styles
    relevant issue: #56
    
    Signed-off-by: 3nt3 <gott@3nt3.de>

--- frontend/src/components/LangChanger.jsx
+++ frontend/src/components/LangChanger.jsx
@@ -2,21 +2,12 @@ import React, { Component } from "react";
 import { withTranslation, I18nextProvider } from "react-i18next";
 import css from "../styles/langChanger.module.scss";
 class LangChanger extends Component {
-  constructor(props) {
-    super(props);
-
-    this.state = {
-      language: "",
-    };
-  }
-
   changeLng(lng) {
     this.props.i18n.changeLanguage(lng);
   }
 
   render() {
     const { t } = this.props;
-
     return (
       <I18nextProvider>
         <div className={css.container}>
@@ -24,26 +15,25 @@ class LangChanger extends Component {
             <span>{t("selectLanguage.language")}</span>
             <p>&nbsp;</p>
             <i className="fas fa-language"></i>
-
             <ul className={css.dropdown}>
-              <li>
-                <p onClick={() => this.changeLng("de")}>
+              <li onClick={() => this.changeLng("de")}>
+                <p>
                   deutsch{" "}
                   <span role="img" aria-label="german">
                     🇩🇪
                   </span>
                 </p>
               </li>
-              <li>
-                <p onClick={() => this.changeLng("en")}>
+              <li onClick={() => this.changeLng("en")}>
+                <p>
                   english{" "}
                   <span role="img" aria-label="english">
                     🇬🇧
                   </span>
                 </p>
               </li>
-              <li>
-                <p onClick={() => this.changeLng("la")}>
+              <li onClick={() => this.changeLng("la")}>
+                <p>
                   lingua latina <span role="img" aria-label="latin"></span>
                 </p>
               </li>
