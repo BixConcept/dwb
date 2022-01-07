f27c221b453ebfe16e5a52f8a66c188888f27fe0
3nt3 <gott@3nt3.de>
2022-01-07 08:40:08 +0100

    remove unnecessary imports
    Signed-off-by: 3nt3 <gott@3nt3.de>

--- src/components/auth/Login.jsx
+++ src/components/auth/Login.jsx
@@ -1,4 +1,4 @@
-import React, { Component, Fragment, useState } from "react";
+import React, { Fragment, useState } from "react";
 import { useSelector, useDispatch } from "react-redux";
 import PasswordShowAndHide from "./PasswordShowAndHide.jsx";
 import "../../styles/auth/auth.scss";
